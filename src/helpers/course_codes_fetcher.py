998433d01bc8c6cd6e7b6260d8e2af2eb122ef26
Shahzad Nizamani <nizamanias124@gmail.com>
2026-03-18 00:25:05 +0500

    canonical course names for cse and csm

--- src/helpers/course_codes_fetcher.py
+++ src/helpers/course_codes_fetcher.py
@@ -1,5 +1,8 @@
 from sqlalchemy import text
 
 def get_course_code(db_session, course):
-    codes = db_session.execute(text("SELECT course_code FROM COURSE WHERE COURSE_NAME = :course"), {"course": course}).fetchall()
-    return tuple([row[0] for row in codes])
+    codes = db_session.execute(
+        text("SELECT course_code FROM course WHERE canonical_name = (SELECT canonical_name FROM course WHERE course_name = :course LIMIT 1)"),
+        {"course": course}
+    ).fetchall()
+    return tuple(row[0] for row in codes)
