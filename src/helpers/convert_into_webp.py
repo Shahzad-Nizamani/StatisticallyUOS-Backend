036566a04724246923f511be9361dac539acd7a7
Shahzad Nizamani <nizamanias124@gmail.com>
2026-03-23 17:19:45 +0500

    updated imcs and some other teachers

--- src/helpers/convert_into_webp.py
+++ src/helpers/convert_into_webp.py
@@ -1,8 +1,12 @@
 from PIL import Image
 from pathlib import Path
 
-src = Path('src/static/images/teachers')
-for jpg in src.glob('*.jpg'):
+# Path relative to the script file (works from any directory)
+src = Path(__file__).parent.parent / 'static/images/teachers'
+
+for jpg in src.glob('*.jpeg'):
     img = Image.open(jpg)
     img.save(jpg.with_suffix('.webp'), 'webp', quality=85)
     print(f'Converted {jpg.name}')
+
+print(f'Done. Looked in: {src}')
--- src/helpers/delete_jpg.py
+++ src/helpers/delete_jpg.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-src = Path('src/static/images/teachers')
+src = Path(__file__).parent.parent / 'static/images/teachers'
 deleted = 0
 
-for jpg in src.glob('*.jpg'):
+for jpg in src.glob('*.jpeg'):
     jpg.unlink()
     print(f'Deleted {jpg.name}')
     deleted += 1
